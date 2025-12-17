4b490e5776c7a788237cb2d2427abc2bc396e843
Jam-al1 <feranmimuhammed2022@gmail.com>
2025-12-17 16:37:08 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const fades = document.querySelectorAll(".fade");
+
+window.addEventListener("scroll", () => {
+  fades.forEach(el => {
+    const pos = el.getBoundingClientRect().top;
+    if (pos < window.innerHeight - 100) {
+      el.classList.add("show");
+    }
+  });
+});
